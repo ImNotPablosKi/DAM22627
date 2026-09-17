00002330d858112c18f859f5cdc77cca9bbaeb27
ImNotPablosKi <pmigueld@iesch.org>
2026-09-17 18:20:09 +0200

    fuhh

--- PMDM-MovilesMultimedia/Android/EdadCanina/app/src/main/java/org/iesch/edadcanina/MainActivity.kt
+++ PMDM-MovilesMultimedia/Android/EdadCanina/app/src/main/java/org/iesch/edadcanina/MainActivity.kt
@@ -1,12 +1,18 @@
 package org.iesch.edadcanina
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n", "StringFormatInvalid")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,8 +24,31 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Aqui la pantalla está creada
+        // 1 - Tomamos el control de todos los elementos de la parte de la UI
 
-        // Tomamos el control de todos los elementos de la parte de la UI
+        val resultText = findViewById<TextView>(R.id.texto_respuesta)
+        val calculateButton = findViewById<Button>(R.id.boton_calcular)
+        val ageEdit = findViewById<EditText>(R.id.edit_edad)
+
+        // 2 - Los botones tienen la propiedad setOnClickListener al pulsarlo
+        calculateButton.setOnClickListener {
+
+            var edadInt = 0
+
+            // Aqui metemos el codigo de lo que queremos hacer cuando pulsamos el boton de calcular
+            val edadString = ageEdit.text.toString()
+
+            if (edadString.isEmpty()) {
+                // Mostramos un mensaje de tipo toast
+                Toast.makeText(this, "Imbécil.", Toast.LENGTH_LONG).show()
+            } else {
+
+               edadInt = edadString.toInt() * 7
+               // Necesitamos pasar el valor a entero
+               resultText.text = getString(R.string.resultado_texto, edadInt)
+
+            }
+        }
 
     }
 }
