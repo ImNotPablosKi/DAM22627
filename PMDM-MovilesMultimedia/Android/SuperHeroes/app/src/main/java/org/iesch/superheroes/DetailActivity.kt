972d1467734c6950fc42e91cbb5a3c019b42a87e
ImNotPablosKi <pmigueld@iesch.org>
2026-09-22 20:24:28 +0200

    yuhh

--- PMDM-MovilesMultimedia/Android/SuperHeroes/app/src/main/java/org/iesch/superheroes/DetailActivity.kt
+++ PMDM-MovilesMultimedia/Android/SuperHeroes/app/src/main/java/org/iesch/superheroes/DetailActivity.kt
@@ -1,6 +1,8 @@
 package org.iesch.superheroes
 
 import android.os.Bundle
+import android.widget.RatingBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,19 @@ class DetailActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Último paso: Recibimos los datos del MainActivity que hemos enviado
+        val bundle = intent.extras!!
+        val superHeroName = bundle.getString("superHeroName") ?: "No hay nombre." // Controlar nulos
+        val alterEgo = bundle.getString("alterEgo") ?: "No hay un alter ego disponible."
+        val bio = bundle.getString("bio") ?: "No hay una biografía disponible."
+        val power = bundle.getFloat("power")
+
+        // Rellenamos los campos con los valores recibidos
+        findViewById<TextView>(R.id.heroName_tv).text = superHeroName
+        findViewById<TextView>(R.id.alterEgoResult).text = alterEgo
+        findViewById<TextView>(R.id.bioResult).text = bio
+        findViewById<RatingBar>(R.id.ratingBar2).rating = power
+
     }
 }
--- PMDM-MovilesMultimedia/Android/SuperHeroes/app/src/main/java/org/iesch/superheroes/MainActivity.kt
+++ PMDM-MovilesMultimedia/Android/SuperHeroes/app/src/main/java/org/iesch/superheroes/MainActivity.kt
@@ -1,6 +1,10 @@
 package org.iesch.superheroes
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RatingBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,38 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // A partir de aquí introduzco el código necesario
+        val botonGuardar = findViewById<Button>(R.id.botonGuardar)
+
+        botonGuardar.setOnClickListener {
+
+            // Obtenemos los valores al momento de hacer click
+            val superHeroName = findViewById<EditText>(R.id.heroNameEdit).text.toString()
+            val alterEgo = findViewById<EditText>(R.id.heroNameEdit2).text.toString()
+            val bio = findViewById<EditText>(R.id.editTextText3).text.toString()
+            val power = findViewById<RatingBar>(R.id.power).rating
+
+            // Qué quiero hacer cuando pulso el botón guardar
+            irADetailActivity(superHeroName, alterEgo, bio, power)
+
+        }
+    }
+
+    fun irADetailActivity(superHeroName: String, alterEgo: String, bio: String, power: Float) {
+
+        // Creamos el objeto Intent
+        val intent = Intent(this, DetailActivity::class.java)
+
+        // Añadimos todos los campos con el método putExtra
+        // De esta manera, todos estos datos se llevarán a la detailActivity
+        intent.putExtra("superHeroName", superHeroName)
+        intent.putExtra("alterEgo", alterEgo)
+        intent.putExtra("bio", bio)
+        intent.putExtra("power", power)
+
+        // Iniciar la actividad
+        startActivity(intent)
+
     }
 }
